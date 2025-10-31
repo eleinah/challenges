cfc88f1e1151b4823aeea75b368dbe81cce9b663
Eleina Hayden <eleina@member.fsf.org>
2025-10-30 23:39:29 -0500

    solve 125 (valid palindrome)
    This is definitely not optimized, but at least it was fun!

---
+++ go/leetcode/125.go
@@ -0,0 +1,24 @@
+func isPalindrome(s string) bool {
+    expr := `[a-zA-Z0-9]`
+    re, _ := regexp.Compile(expr)
+
+    scrubs := re.FindAllString(s, -1)
+    scrubbed := ""
+    for _, scrub := range scrubs {
+        scrubbed += strings.ToLower(scrub)
+    }
+    scrubbed = strings.ReplaceAll(scrubbed, " ", "")
+    rev := reverse(scrubbed)
+
+    fmt.Println(scrubbed)
+    fmt.Println(rev)
+
+    return scrubbed == rev
+}
+
+func reverse(s string) (result string) {
+    for _, c := range s {
+        result = string(c) + result
+    }
+    return
+}
